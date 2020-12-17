b458d915f58920ad0516673d02760da940c279d9
SushilGautam <sushilgautam40@gmail.com>
2020-12-16 22:17:34 -0500

    pychon file to give html created

---
+++ myfirstpython.py
@@ -0,0 +1,13 @@
+print(
+    """
+    <html>
+        <head>
+            <title>My Web</title>
+        </head>
+        <body>
+            <h1>Hello World</h1>
+        </body>
+    </html>
+
+"""
+)
